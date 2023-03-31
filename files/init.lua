4a31e0ae179157604c6f5c90fe8e47321da89f44
jahrik <jahrik@gmail.com>
2023-03-31 02:27:14 -0700

    More tests and linting

--- files/init.lua
+++ files/init.lua
@@ -1,3 +1,9 @@
 require("conf.remap")
 require("conf.plugins")
 require("conf.set")
+
+local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+for type, icon in pairs(signs) do
+  local hl = "LspDiagnosticsSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
