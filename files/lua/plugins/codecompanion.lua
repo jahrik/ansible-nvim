4be54a4fbec80ff050c051e96ec1166b297fdb9d
Wesley Gill <wgill@ignite-retail.com>
2026-06-15 14:26:29 -0700

    Add an inline prompt

--- files/lua/plugins/codecompanion.lua
+++ files/lua/plugins/codecompanion.lua
@@ -9,6 +9,7 @@ return {
 		{ "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, desc = "Toggle chat" },
 		{ "<leader>ai", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, desc = "Actions menu" },
 		{ "<leader>aa", "<cmd>CodeCompanionChat Add<cr>", mode = "v", desc = "Add to chat" },
+		{ "<leader>ap", "<cmd>CodeCompanion<cr>", mode = { "n", "v" }, desc = "Inline prompt" },
 	},
 	opts = {
 		strategies = {
